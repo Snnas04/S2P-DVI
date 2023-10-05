de59caa55d2a373fb08b7beb9949be923e388a98
Marc <marcsans26@gmail.com>
2023-10-05 18:42:32 +0200

    test

--- HelloWorld/main.js
+++ HelloWorld/main.js
@@ -1,13 +1,13 @@
 const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-  
-    win.loadFile('index.html')
-  }
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadURL('https://www.electronjs.org/')
+}
 
 app.whenReady().then(() => {
 createWindow()
