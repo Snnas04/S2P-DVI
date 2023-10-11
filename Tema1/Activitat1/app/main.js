700af9f91264a406d82097dda648ca27ac9c1afc
Marc <marcsans26@gmail.com>
2023-10-11 16:59:27 +0200

    avansos amb l'interficie

--- Tema1/Activitat1/app/main.js
+++ Tema1/Activitat1/app/main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 1440,
-        height: 1080
+        width: 1290,
+        height: 920
     })
 
     win.loadFile('index.html')
