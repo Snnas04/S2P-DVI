07d30a386eeecdc41237a06bbe5770d2d8deb302
Marc <marcsans26@gmail.com>
2023-10-13 20:41:10 +0200

    acabar practica

--- Tema1/Activitat1/app/main.js
+++ Tema1/Activitat1/app/main.js
@@ -1,14 +1,21 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
+const {teplate} = require('./menu')
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 1290,
-        height: 920
-    })
+  const win = new BrowserWindow({
+    width: 1290,
+    height: 920
+  })
 
-    win.loadFile('index.html')
-    }
+  const menu = Menu.buildFromTemplate(teplate)
+
+  win.loadFile('index.html')
+
+  Menu.setApplicationMenu(menu)
+
+  win.setMenu(menu)
+}
 
 app.whenReady().then(() => {
-    createWindow()
+  createWindow()
 })
