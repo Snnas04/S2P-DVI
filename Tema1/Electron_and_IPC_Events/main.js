f7799bd13be2fdbb5d9c9f05b9e45e1618c094ac
Marc <marcsans26@gmail.com>
2023-10-19 17:30:12 +0200

    activitat 3

--- Tema1/Electron_and_IPC_Events/main.js
+++ Tema1/Electron_and_IPC_Events/main.js
@@ -2,9 +2,9 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-    width: 800,
-    height: 600
-    })
+        width: 1290,
+        height: 920
+      })
 
     win.loadFile('index.html')
 }
