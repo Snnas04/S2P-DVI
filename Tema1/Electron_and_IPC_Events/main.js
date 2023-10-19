ec99a3126882e15a6d5eeb1c08975b20ce91b04e
Marc <marcsans26@gmail.com>
2023-10-19 19:04:35 +0200

    exercisi3

--- Tema1/Electron_and_IPC_Events/main.js
+++ Tema1/Electron_and_IPC_Events/main.js
@@ -1,14 +1,30 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('node:path')
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 1290,
-        height: 920
-      })
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
 
-    win.loadFile('index.html')
+  ipcMain.on('set-title', (event, title) => {
+    const webContents = event.sender
+    const win = BrowserWindow.fromWebContents(webContents)
+    win.setTitle(title)
+  })
+
+  mainWindow.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-    createWindow()
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
 })
