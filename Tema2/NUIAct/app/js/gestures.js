9bc3518a4dcff55387f5a0e56cccf0f488263e5b
Marc <marcsans26@gmail.com>
2023-11-15 16:50:11 +0100

    funcionen els tres gestes

--- Tema2/NUIAct/app/js/gestures.js
+++ Tema2/NUIAct/app/js/gestures.js
@@ -48,20 +48,19 @@ const config = {
         one.addCurl(finger, fp.FingerCurl.HalfCurl, 1.0);
     }
 
-
     for (let finger of [fp.Finger.Index]) {
       one.addCurl(finger, fp.FingerCurl.NoCurl, 1.0);
       one.addDirection(finger, fp.FingerDirection.VerticalUp, 1.0);
       one.addDirection(finger, fp.FingerDirection.DiagonalUpLeft, 1.0);
       one.addDirection(finger, fp.FingerDirection.DiagonalUpRight, 1.0);
-  }
+    }
     
     // two
     const two = new fp.GestureDescription('two');
 
     for (let finger of [fp.Finger.Thumb, fp.Finger.Ring, fp.Finger.Pinky]) {
-        two.addCurl(finger, fp.FingerCurl.FullCurl, 1.0);
-        two.addCurl(finger, fp.FingerCurl.HalfCurl, 1.0);
+        two.addCurl(finger, fp.FingerCurl.FullCurl, 0.9);
+        two.addCurl(finger, fp.FingerCurl.HalfCurl, 0.9);
     }
 
     for (let finger of [fp.Finger.Index, fp.Finger.middle]) {
