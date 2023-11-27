b9f847a981b40f9259b1216b4301997d8730c764
Marc <marcsans26@gmail.com>
2023-11-27 21:03:06 +0100

    nova app amb react

---
+++ Tema3/my-app/public/main.js
@@ -0,0 +1,16 @@
+const { BrowserWindow } = require('electron').remote
+require('@electron/remote/main').initialize()
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            enableRemoteModule: true,
+        }
+    })
+
+    win.loadURL(`http://localhost:3000`)
+}
+
+app.whenReady().then(createWindow)
