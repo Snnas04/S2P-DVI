9ed20089abdbeaaba75c66c80226ac612ab85f81
Marc <marcsans26@gmail.com>
2024-02-23 15:47:43 +0100

    testing

--- FinalDeCurs/node-testing/index.js
+++ FinalDeCurs/node-testing/index.js
@@ -2,4 +2,16 @@ const sum = (a, b) => {
     return a + b;
 }
 
-module.exports = { sum };
+const rest = (a, b) => {
+    return a - b;
+}
+
+const mult = (a, b) => {
+    return a * b;
+}
+
+const div = (a, b) => {
+    return b === 0 ? "Error" : a / b;
+}
+
+module.exports = { sum, rest, mult, div };
--- FinalDeCurs/node-testing/index.test.js
+++ FinalDeCurs/node-testing/index.test.js
@@ -1,5 +1,41 @@
-const { sum } = require('./index.js');
+const { sum, rest, mult, div } = require('./index.js');
 
+// suma
 test('simple sum test', () => {
     expect(sum(1, 2)).toBe(3);
+})
+
+test('sum with negative numbers test', () => {
+    expect(sum(-1, 2)).toBe(1);
+})
+
+// resta
+test('simple rest test', () => {
+    expect(rest(4, 1)).toBe(3);
+})
+
+test('rest with negative numbers test', () => {
+    expect(rest(-1, 2)).toBe(-3);
+})
+
+// multiplicación
+test('simple mult test', () => {
+    expect(mult(6, 2)).toBe(12);
+})
+
+test('mult with negative numbers test', () => {
+    expect(mult(-1, 2)).toBe(-2);
+})
+
+// división
+test('simple div test', () => {
+    expect(div(6, 2)).toBe(3);
+})
+
+test('div by 0 test', () => {
+    expect(div(6, 0)).toBe("Error");
+})
+
+test('div with negative numbers test', () => {
+    expect(div(-6, 2)).toBe(-3);
 })
